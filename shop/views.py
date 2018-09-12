0bb3d786115bd4573dc3df91e419e21a123902f7
Bishnu Bhusal <bishnu.bhusal111@gmail.com>
2018-09-12 09:20:04 +0545

    slider added

--- shop/views.py
+++ shop/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render, redirect
 from .forms import SignUpForm
+from .models import Slider
 
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'shop/index.html')
+    slides = Slider.objects.all()
+    context = {
+        'slides': slides
+    }
+    return render(request, 'shop/index.html', context)
 
 
 def test(request):
