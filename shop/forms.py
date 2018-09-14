2deca9b75652c7164492401d513176d10a3cdbae
Bishnu Bhusal <bishnu.bhusal111@gmail.com>
2018-09-14 08:52:34 +0545

    review form added

--- shop/forms.py
+++ shop/forms.py
@@ -1,7 +1,16 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.forms import ModelForm
+from .models import Review
+
 
 class SignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+
+class ReviewForm(ModelForm):
+    class Meta:
+        model = Review
+        fields = ['rating', 'comment']
--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .forms import SignUpForm
+from .forms import SignUpForm, ReviewForm
 from .models import Slider, Product, Category
 
 
@@ -37,10 +37,20 @@ def register(request):
 
 def product(request, product_slug):
     pro = Product.objects.get(slug=product_slug)
+    form = ReviewForm()
+    if request.method == 'POST':
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            review = form.save(commit=False)
+            review.product = pro
+            review.user = request.user
+            review.save()
+
     categories = Category.objects.order_by('title').all()
     context = {
         'product': pro,
-        'categories': categories
+        'categories': categories,
+        'form': form
     }
 
     return render(request, 'shop/product.html', context)
